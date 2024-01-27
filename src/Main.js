49178312ff9594c6c9ba6046b62074deb1a8beb7
Mèo Lười Cute <80211558+Meoluoicute2000@users.noreply.github.com>
2024-01-27 17:14:09 +0700

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -9,14 +9,13 @@ const bot = new Bot(config);
 //Discord.js 13.6.0
 
 //Hosting Web
-
-//const app = express();
-//const port = 3000;
-//app.get('/', (req, res) => {
-//  const imagePath = path.join(__dirname, 'index.html');
-//  res.sendFile(imagePath);
-//});
-//app.listen(port, () => console.log('\x1b[36m%s\x1b[0m', `|    😾 Lắng nghe cổng Cherry : ${port}`));
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  const imagePath = path.join(__dirname, 'index.html');
+  res.sendFile(imagePath);
+});
+app.listen(port, () => console.log(`|  🔗 Cherry đang mở cổng: ${port}`));
 
 bot.start();
 
